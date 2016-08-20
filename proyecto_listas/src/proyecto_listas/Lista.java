35096cd1a355de665af221ff20118b4532865c13
lesterarte <lesterarte007@unitec.edu>
2016-08-20 16:14:23 -0600

    terminando lista de alumnos

--- proyecto_listas/src/proyecto_listas/Lista.java
+++ proyecto_listas/src/proyecto_listas/Lista.java
@@ -3,5 +3,30 @@ package proyecto_listas;
 
 
 public class Lista {
+    private Nodo head = null;     
+    private class Nodo{
+        public Alumno alumno; 
+        public Nodo next = null; 
+        public Nodo(Alumno alumno) {
+            this.alumno = alumno;
+        }
+    }
+    
+    
+    public void insert(Alumno alum){
+        
+        Nodo nuevo = new Nodo(alum); 
+        if(this.head == null){
+            nuevo.next = head;   
+            head = nuevo; 
+        }
+        else {
+            Nodo puntero = head; 
+            while(puntero.next != null ){
+                puntero = puntero.next;                
+            }
+        }
+                       
+    }
     
 }
